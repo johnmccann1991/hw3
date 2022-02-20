4c45596d194414e68fae18256f1a4cc5a7152ebe
johnmccann1991 <john.mccann@kellogg.northwestern.edu>
2022-02-20 18:43:40 +0000

    saving

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
   Rails.application.routes.draw do
     resources :places
     resources :posts
+  
+    root "places#index"
+
   end
+
+
   
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
