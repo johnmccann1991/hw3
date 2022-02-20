b167277043854cf344d0d014dd5b675b9c1a560e
johnmccann1991 <john.mccann@kellogg.northwestern.edu>
2022-02-20 19:19:46 +0000

    place and post controller stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,30 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+        @place = Place.find(parans["place_id"])
+        @post.place_id = @place.id
+    end
+
+    def create
+        @post = Post.new(parans["post"])
+        @post.save
+        redirect_to "/places/#{@post.place_id}"
+    end
+
+    def show
+        @post = Post.find(parans["id"])
+    end
+
+end
+
+
+       
+
+
+
+
 end
