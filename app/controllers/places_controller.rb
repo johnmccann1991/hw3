35c5ae62310acd92af5ea8732a67df92fad0666b
johnmccann1991 <john.mccann@kellogg.northwestern.edu>
2022-02-20 18:57:35 +0000

    places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,20 @@
 class PlacesController < ApplicationController
+    def index
+        @places = Place.all
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(parans["place"])
+        @place.save
+        redirect_to "/places"
+    end
+
+    def show
+        @place = Place.find(parans["id"])
+    end
+
 end
